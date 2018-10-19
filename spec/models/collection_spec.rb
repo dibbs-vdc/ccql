ca3bc0af6a493de2b495cb29a4486c0fdd3a95ba
Tom Johnson <tom@curationexperts.com>
2018-10-18 17:43:51 -0700

    Add `#collection_size` vocabulary
    Adds the controlled vocabulary configuration and QA service for
    `Collection#collection_size`.
    
    Connected to #3.

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Collection do
-  it "has tests" do
-    skip "Add your tests here"
+  subject(:collection) { described_class.new(title: ['Comet in Moominland']) }
+
+  describe '#collection_size' do
+    it 'handles expected values' do
+      expect { collection.collection_size = '1_gb' }
+        .not_to change { collection.valid? }
+        .from true
+    end
   end
 end
