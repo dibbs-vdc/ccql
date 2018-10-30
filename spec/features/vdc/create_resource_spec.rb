ebfd54865a7ec8852a2eee64dce1ffccbf265314
kellyChex <kelly@notch8.com>
2018-10-29 19:58:20 -0700

    Admin set was not being created properly and user did not have right access so fixed that in this test

--- spec/features/vdc/create_resource_spec.rb
+++ spec/features/vdc/create_resource_spec.rb
@@ -1,27 +1,23 @@
 # Generated via
 #  `rails generate hyrax:work Vdc::Resource`
 require 'rails_helper'
-include Warden::Test::Helpers
 
 # NOTE: If you generated more than one work, you have to set "js: true"
 RSpec.feature 'Create a Vdc::Resource', js: false do
   context 'a logged in user' do
-    let(:user_attributes) do
-      { email: 'test@example.com' }
-    end
-    let(:user) do
-      User.new(user_attributes) { |u| u.save(validate: false) }
-    end
+    let!(:user) { create(:admin_user, password: 'testing123') }
 
     before do
-      AdminSet.find_or_create_default_admin_set_id
       login_as user
+      visit '/admin/admin_sets/new'
+      page.fill_in 'Title', with: 'Default Admin Set'
+      page.fill_in 'Abstract or Summary', with: 'WHAT is your quest?'
+      click_button 'Save'
     end
 
     scenario do
-      visit '/dashboard'
-      click_link "Works"
-      click_link "Add new work"
+      visit '/dashboard/my/works'
+      page.click_link('Add new work')
 
       # If you generate more than one work uncomment these lines
       # choose "payload_concern", option: "Vdc::Resource"
