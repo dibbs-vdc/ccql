9c80c0f9621bc22a1ddf776c6794c50fa1bf72b4
kellyChex <kelly@notch8.com>
2018-11-14 13:11:46 -0800

    Make user attributes a constant

--- app/controllers/hyrax/dashboard/profiles_controller.rb
+++ app/controllers/hyrax/dashboard/profiles_controller.rb
@@ -6,9 +6,16 @@ module Hyrax
       before_action :find_user
       authorize_resource class: '::User', instance_name: :user
 
+      USER_ATTRIBUTES = [:first_name, :last_name, :organization, :organization_other, 
+      :department, :position, :position_other, {:discipline => []}, :discipline_other, 
+      :orcid, :address, :email, :cv_link, :cv_file, :website,:sites_open_science_framework,
+      :sites_open_science_framework_url, :sites_researchgate, :sites_researchgate_url,
+      :sites_linkedin, :sites_linkedin_url,:sites_vivo, :sites_vivo_url, :sites_institutional_repo,
+      :sites_institutional_repo_url, :sites_other, :sites_other_url].freeze
+
       # Process changes from profile form
       def update
-        if conditionally_update
+        if update_user
           redirect_to hyrax.dashboard_profile_path(@user.to_param), notice: "Your profile has been updated"
         else
           redirect_to hyrax.edit_dashboard_profile_path(@user.to_param), alert: @user.errors.full_messages
@@ -17,35 +24,14 @@ module Hyrax
 
       private
 
-        # Update user if they sent user params, otherwise return true.
-        # This is important because this controller is also handling removing trophies.
-        # (but we should move that to a different controller)
-        def conditionally_update
-          return true unless params[:user]
-          @user.update(user_params)
-        end
+      def update_user
+        return true unless params[:user]
+        @user.update(user_params)
+      end
 
-        def user_params
-          params.require(:user).permit(key_attributes)
-        end
-  
-        def key_attributes
-          [:first_name, :last_name, 
-           :organization, :organization_other, 
-           :department,
-           :position, :position_other, 
-           {:discipline => []}, :discipline_other, 
-           :orcid,
-           :address, :email,
-           :cv_link, :cv_file, 
-           :website,
-           :sites_open_science_framework, :sites_open_science_framework_url,
-           :sites_researchgate, :sites_researchgate_url,
-           :sites_linkedin, :sites_linkedin_url,
-           :sites_vivo, :sites_vivo_url,
-           :sites_institutional_repo, :sites_institutional_repo_url,
-           :sites_other, :sites_other_url]
-        end
+      def user_params
+        params.require(:user).permit(USER_ATTRIBUTES)
+      end
     end
   end
 end
