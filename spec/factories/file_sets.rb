0cea98db26f9560f17f2d33c46b93edc822b9650
Benjamin Kiah Stroud <kiah@notch8.com>
2018-12-12 20:58:19 +0000

    I12 Default Thumbnails by File Type

---
+++ spec/factories/file_sets.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :file_set do
+    transient do
+      content { nil }
+    end
+    after(:create) do |file, evaluator|
+      Hydra::Works::UploadFileToFileSet.call(file, evaluator.content) if evaluator.content
+    end
+  end
+end
