477e938725dd2c044edf1fdd2fa8e0328518cc13
Lea Ann Bradford <leaann@notch8.com>
2019-06-25 09:58:49 -0700

    add controller action for new usages form

---
+++ app/controllers/hyrax/vdc/usages_controller.rb
@@ -0,0 +1,23 @@
+module Hyrax
+  class Vdc::UsagesController < ApplicationController
+    
+    def create
+      @vdc_usage = ::Vdc::Usage.new(usage_params)
+      if @vdc_usage.save
+        respond_to do |format|
+          format.html { redirect_to @vdc_usage.href }
+          format.js 
+          format.json { render json: @vdc_usage, status: :created }
+        end        
+      else
+        render json: { errors: @vdc_usage.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+      def usage_params
+        params.require(:vdc_usage).permit(:purpose, :user_id, :work_id, :href)
+      end
+  end
+end
