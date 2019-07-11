140eb42860b7871417a1dc3d545bfa81193160b2
Lea Ann Bradford <leaann@notch8.com>
2019-07-11 14:43:59 -0700

    comment out some tests

--- spec/services/vdc/resource_usage_spec.rb
+++ spec/services/vdc/resource_usage_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Vdc::ResourceUsage do
+RSpec.describe Vdc::Usage do
   subject(:service) { described_class.new(resource: resource) }
   let(:resource)    { FactoryBot.create(:vdc_resource) }
 
