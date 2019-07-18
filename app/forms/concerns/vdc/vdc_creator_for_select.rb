46928364aa52498a7d47cb28108b67e853821bd4
Rob Kaufman <rob@notch8.com>
2019-07-17 21:45:52 -0700

    by default search returns 10 rows, I think this is why it works locally (where there are 2-3 users) but not in prod

--- app/forms/concerns/vdc/vdc_creator_for_select.rb
+++ app/forms/concerns/vdc/vdc_creator_for_select.rb
@@ -5,7 +5,7 @@ module Vdc::VdcCreatorForSelect
   # TODO: Remove if you're able to get this generated via QA
   def vdc_creator_for_select
     field_list = "id, preferred_name_tesim"
-    select_result = Blacklight.default_index.connection.select(params: { q: "*:*", fq: "has_model_ssim:\"Vdc::Person\"", fl: field_list })
+    select_result = Blacklight.default_index.connection.select(params: { q: "*:*", fq: "has_model_ssim:\"Vdc::Person\"", fl: field_list, rows: 100000 })
 
     select_collection = []
     select_result['response']['docs'].each do |doc|
