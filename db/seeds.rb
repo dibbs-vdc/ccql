9a6727de46321284181b191b4eb631f6c4eaf278
Rob Kaufman <rob@notch8.com>
2019-07-19 12:26:04 -0700

    working on missing translations

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,3 +37,12 @@ if Rails.env.development?
 
   factory.actor_create(attributes: attributes)
 end
+
+collection_types = Hyrax::CollectionType.all
+collection_types.each do |c|
+  next unless c.title =~ /^translation missing/
+  oldtitle = c.title
+  c.title = I18n.t(c.title.gsub("translation missing: en.", ''))
+  c.save
+  puts "#{oldtitle} changed to #{c.title}"
+end
