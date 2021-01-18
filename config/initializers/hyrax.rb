08ac5e378637a8be5b846513d72487fdf93c4af1
jzgo <jez@notch8.com>
2021-01-18 13:52:49 -0800

    configure upload path

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -126,7 +126,7 @@ Hyrax.config do |config|
 
   # Temporary paths to hold uploads before they are ingested into FCrepo
   # These must be lambdas that return a Pathname. Can be configured separately
-  #  config.upload_path = ->() { Rails.root + 'tmp' + 'uploads' }
+  config.upload_path = ->() { Rails.root + 'tmp' + 'uploads' }
   #  config.cache_path = ->() { Rails.root + 'tmp' + 'uploads' + 'cache' }
 
   # Location on local file system where derivatives will be stored
