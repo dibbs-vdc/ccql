633695b4736bd7d8d35ec75a1b5c5e6afe9248bb
Rob Kaufman <rob@notch8.com>
2021-05-04 16:02:35 -0700

    update reset from other projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminSet.find_or_create_default_admin_set_id
-Rake::Task['hyrax:default_collection_types:create'].invoke
+puts "\n== Creating default collection types"
+Hyrax::CollectionType.find_or_create_admin_set_type
+Hyrax::CollectionType.find_or_create_default_collection_type
+
+collection_types = Hyrax::CollectionType.all
+collection_types.each do |c|
+  next unless c.title =~ /^translation missing/
+  oldtitle = c.title
+  c.title = I18n.t(c.title.gsub("translation missing: en.", ''))
+  c.save
+  puts "#{oldtitle} changed to #{c.title}"
+end
+
+Hyrax::Workflow::WorkflowImporter.load_workflows
+errors = Hyrax::Workflow::WorkflowImporter.load_errors
+abort("Failed to process all workflows:\n  #{errors.join('\n  ')}") unless errors.empty?
 
 if Rails.env.development?
   users = ['archivist@example.com', 'admin@example.com', 'approved@example.com', 'rob@notch8.com']
@@ -37,13 +52,4 @@ if Rails.env.development?
                      visibility:   'open' }
 
   factory.actor_create(attributes: attributes)
-end
-
-collection_types = Hyrax::CollectionType.all
-collection_types.each do |c|
-  next unless c.title =~ /^translation missing/
-  oldtitle = c.title
-  c.title = I18n.t(c.title.gsub("translation missing: en.", ''))
-  c.save
-  puts "#{oldtitle} changed to #{c.title}"
 end
